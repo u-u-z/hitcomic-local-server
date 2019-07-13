ad2ae31f7d7df3d3281c1ad03d7110fead0e05b7
lrtray <JiA-Aoturay@outlook.com>
2019-07-13 21:21:52 +0800

    Add QueryService.
    Add QueryService.

--- service.go
+++ service.go
@@ -6,6 +6,10 @@ import "regexp"
 type SafeService struct {
 }
 
+// QueryService ...
+type QueryService struct{
+}
+
 // CheckKey ...
 func (service *SafeService) CheckKey(key string) (bool, error) {
 	return regexp.MatchString("[a-zA-Z0-9]{32}", key)
@@ -16,4 +20,12 @@ func (service *SafeService) CheckToken(token string) (bool, error) {
 	return regexp.MatchString("\\w{8}(-\\w{4}){3}-\\w{12}", token)
 }
 
-//
+// QueryTicket ...
+func (service *QueryService) QueryTicket(key string) {
+	var ticket []Tickets
+	query := db.where("Key = ?",key).find(&ticket) //Get all matching records
+	query.Error != nil{
+		panic(query.Error)
+	}
+	return ticket
+}
