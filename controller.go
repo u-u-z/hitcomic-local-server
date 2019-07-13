bc3c91d67780442500a723a3feeddd2a1bf6a09c
RemiIO <i@linux.dog>
2019-07-14 02:56:11 +0800

    mv safe service as function to controller.

--- controller.go
+++ controller.go
@@ -1,5 +1,17 @@
 package main
 
+import "regexp"
+
+// CheckKey ...
+func CheckKey(key string) (bool, error) {
+	return regexp.MatchString("[a-zA-Z0-9]{32}", key)
+}
+
+// CheckToken ...
+func CheckToken(token string) (bool, error) {
+	return regexp.MatchString("\\w{8}(-\\w{4}){3}-\\w{12}", token)
+}
+
 // QueryController ...
 type QueryController struct {
 	server *Server
