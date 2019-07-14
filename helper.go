fa40ad30d3f973a3d9deaf84777378614d0a2b6b
RemiIO <i@linux.dog>
2019-07-15 01:45:22 +0800

    Creat Helper: CreateLog.

--- helper.go
+++ helper.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"regexp"
+	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 // CheckKey ...
@@ -12,4 +15,17 @@ func CheckKey(key string) (bool, error) {
 // CheckToken ...
 func CheckToken(token string) (bool, error) {
 	return regexp.MatchString("\\w{8}(-\\w{4}){3}-\\w{12}", token)
+}
+
+// CreateLog ...
+func CreateLog(db *gorm.DB, key string, result uint, info string) {
+	log := Logs{
+		Key:    key,
+		Result: result,
+		Info:   info,
+		BasicModel: BasicModel{
+			CreatedAt: time.Now(),
+		},
+	}
+	db.Create(&log)
 }
