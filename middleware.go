a80a3fdd0866ef270b077c0bf54d8e00dacc4a34
RemiIO <i@linux.dog>
2019-07-17 02:37:44 +0800

    Add Cors() middleware.

--- middleware.go
+++ middleware.go
@@ -183,5 +183,6 @@ func Cors() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Next()
 	}
 }
